c8307db6e11be6f76ac89e84a9ee16e979bb5993
convivial-reducer-7231 <tradford10@gmail.com>
2020-10-05 02:48:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
-  return string.toUpperCase()
 }
 function logWhisper(string) {
-  return string.toLowerCase()
 }
