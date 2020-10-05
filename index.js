e0123e8e74b2314b2859accf160687450facb1c6
convivial-reducer-7231 <tradford10@gmail.com>
2020-10-05 04:21:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 function sayHiToGrandma(string) {
-  var lowercase = "I can't hear you!"
-  lowercase.toLowerCase() === lowercase
-  var uppercase = "Yes indeed!"
-  uppercase.toUpperCase() === uppercase
-  var mixedCase = "I love you, too"
-  mixedcase.toMixedCase() === mixedcase
+  if (string = .toLowerCase()) {
+    return "I can't hear you!";
+  } else if (string = .toUpperCase()) {
+      return "Yes indeed!";
+  } else if (string = "I love you, Grandma.") {
+      return "I love you, too.";
     }
 }
