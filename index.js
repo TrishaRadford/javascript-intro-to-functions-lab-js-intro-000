aa123441cc27265b115a576549927f7f55ca3f41
convivial-reducer-7231 <tradford10@gmail.com>
2020-10-05 13:57:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 function sayHiToGrandma(string) {
-  var string = "I can't hear you!";
-  if (string === string.toLowerCase()){
+  var string = .toLowerCase();
+  if (string === string.toLowerCase) {
     return "I can't hear you!";
   }
 }
